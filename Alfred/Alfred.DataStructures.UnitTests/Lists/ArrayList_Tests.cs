8eaafa34e05d5f70cf2d8e645f1c6daaf865684d
g-kasa <167296777+g-kasa@users.noreply.github.com>
2024-11-29 18:17:46 +0100

    Added ArrayList & tests

---
+++ Alfred/Alfred.DataStructures.UnitTests/Lists/ArrayList_Tests.cs
@@ -0,0 +1,165 @@
+﻿using Alfred.DataStructures.Lists;
+
+namespace Alfred.DataStructures.UnitTests.Lists
+{
+    public class ArrayList_Tests
+    {
+        [Fact]
+        public void ArrayList_Insert_TestLengthIncrementWithValidPositionIndex()
+        {
+            // Arrange
+            var list = new ArrayList<int>();
+            var positionIndex = 0;
+            var value = 1;
+
+            // Act
+            list.Insert(positionIndex, value);
+            
+            // Assert
+            Assert.Equal(1, list.Length);
+        }
+
+        [Fact]
+        public void ArrayList_Insert_TestLengthIncrementWithNegativePositionIndex()
+        {
+            // Arrange
+            var list = new ArrayList<int>();
+            var positionIndex = -1;
+            var value = 1;
+
+            // Act
+            var action = () => list.Insert(positionIndex, value);
+
+            // Assert
+            Assert.Throws<IndexOutOfRangeException>(action);
+        }
+
+        [Fact]
+        public void ArrayList_Insert_TestLengthIncrementWithPositionIndexGreaterThanLength()
+        {
+            // Arrange
+            var list = new ArrayList<int>();
+            var positionIndex = 1;
+            var value = 1;
+
+            // Act
+            list.Insert(positionIndex, value);
+
+            // Assert
+            Assert.Equal(1, list.Length);
+        }
+
+        [Fact]
+        public void ArrayList_Insert_TestLengthIncrementWithPositionIndexGreaterThanCapacity()
+        {
+            // Arrange
+            var list = new ArrayList<int>();
+            var positionIndex = 10;
+            var value = 1;
+
+            // Act
+            list.Insert(positionIndex, value);
+            
+            // Assert
+            Assert.Equal(1, list.Length);
+        }
+
+        [Fact]
+        public void ArrayList_Insert_TestValueInsertedInTheMiddle()
+        {
+            // Arrange
+            var list = new ArrayList<int>();
+            list.Insert(0, 1);
+            list.Insert(1, 2);
+            list.Insert(2, 3);
+            list.Insert(3, 4);
+            list.Insert(4, 5);
+            var positionIndex = 2;
+            var value = 10;
+
+            // Act
+            list.Insert(positionIndex, value);
+            
+            // Assert
+            Assert.Equal(value, list[positionIndex]);
+            Assert.Equal(5, list[5]);
+            Assert.Equal(4, list[4]);
+            Assert.Equal(3, list[3]);
+        }
+
+        [Fact]
+        public void ArrayList_Insert_TestInsertionOfMoreValuesThanCapacity()
+        {
+            // Arrange
+            var list = new ArrayList<int>(2);
+
+            // Act
+            list.Insert(0, 1);
+            list.Insert(1, 2);
+            list.Insert(2, 3);
+            list.Insert(3, 4);
+            list.Insert(4, 5);
+
+            // Assert
+            Assert.Equal(5, list.Length);
+        }
+
+        [Fact]
+        public void ArrayList_RemoveAt_TestLengthDecrement()
+        {
+            // Arrange
+            var list = new ArrayList<int>();
+            list.Insert(0, 1);
+            list.Insert(1, 2);
+            list.Insert(2, 3);
+            list.Insert(3, 4);
+            list.Insert(4, 5);
+            var positionIndex = 2;
+
+            // Act
+            list.RemoveAt(positionIndex);
+            
+            // Assert
+            Assert.Equal(4, list.Length);
+        }
+
+        [Fact]
+        public void ArrayList_RemoveAt_TestValueRemovedIsReturned()
+        {
+            // Arrange
+            var list = new ArrayList<int>();
+            list.Insert(0, 1);
+            list.Insert(1, 2);
+            list.Insert(2, 3);
+            list.Insert(3, 4);
+            list.Insert(4, 5);
+            var positionIndex = 2;
+            
+            // Act
+            var value = list.RemoveAt(positionIndex);
+
+            // Assert
+            Assert.Equal(3, value);
+        }
+
+        [Fact]
+        public void ArrayList_RemoveAt_TestLeftShiftingForRemainingValues()
+        {
+            // Arrange
+            var list = new ArrayList<int>();
+            list.Insert(0, 1);
+            list.Insert(1, 2);
+            list.Insert(2, 3);
+            list.Insert(3, 4);
+            list.Insert(4, 5);
+            var positionIndex = 2;
+
+            // Act
+            list.RemoveAt(positionIndex);
+            
+            // Assert
+            Assert.Equal(4, list[2]);
+            Assert.Equal(5, list[3]);
+        }
+    }
+}
---
+++ Alfred/Alfred.DataStructures/Lists/ArrayList.cs
@@ -0,0 +1,128 @@
+﻿namespace Alfred.DataStructures.Lists
+{
+    public class ArrayList<T>
+    {
+        /// <summary>
+        /// Collection of items.
+        /// </summary>
+        private T[] Items { get; set; }
+        /// <summary>
+        /// The minimum capacity of the collection.
+        /// </summary>
+        private int MinCapacity { get; set; }
+
+        /// <summary>
+        /// Number of items in the collection.
+        /// </summary>
+        public int Length { get; private set; }
+
+        public ArrayList()
+            : this(8)
+        { }
+
+        public ArrayList(int minCapacity)
+        {
+            MinCapacity = minCapacity;
+            Items = new T[minCapacity];
+            Length = 0;
+        }
+
+        /// <summary>
+        /// Indexer for the collection.
+        /// </summary>
+        /// <paramref name="index">The index of the item to get or set.</paramref>
+        public T this[int index]
+        {
+            get
+            {
+                return Items[index];
+            }
+            set
+            {
+                Items[index] = value;
+            }
+        }
+
+        /// <summary>
+        /// Inserts a value at the specified index.
+        /// </summary>
+        /// <param name="index">
+        /// The index at which to insert the value.
+        /// </param>
+        /// <param name="value">
+        /// The value to insert.
+        /// </param>
+        public void Insert(int index, T value)
+        {
+            if (Length == Items.Length)
+            {
+                DoubleCapacity();
+            }
+
+            var correctIndex = CalculateCorrectIndex(index);
+            ShiftRight(correctIndex);
+
+            Items[correctIndex] = value;
+            Length++;
+        }
+
+        /// <summary>
+        /// Removes the value at the specified index.
+        /// </summary>
+        /// <param name="index">
+        /// The index of the value to remove.
+        /// </param>
+        /// <returns>
+        /// The value that was removed.
+        /// </returns>
+        public T RemoveAt(int index)
+        {
+            var correctIndex = CalculateCorrectIndex(index);
+            var value = Items[correctIndex];
+            ShiftLeft(correctIndex);
+            Length--;
+
+            return value;
+        }
+
+        private void DoubleCapacity()
+        {
+            var newItems = new T[Items.Length * 2];
+            for (var i = 0; i < Items.Length; i++)
+            {
+                newItems[i] = Items[i];
+            }
+            Items = newItems;
+        }
+
+        private void ShiftLeft(int index)
+        {
+            for (var i = index; i < Length; i++)
+            {
+                Items[i] = Items[i + 1];
+            }
+        }
+
+        private void ShiftRight(int index)
+        {
+            for (var i = Length; i > index; i--)
+            {
+                Items[i] = Items[i - 1];
+            }
+        }
+
+        private int CalculateCorrectIndex(int index)
+        {
+            var correctIndex = index;
+            if (correctIndex < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            if (correctIndex > Length)
+            {
+                correctIndex = Length;
+            }
+            return correctIndex;
+        }
+    }
+}
